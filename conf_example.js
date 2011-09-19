ae15ab672e26ddae3b984a4664514c0b5ae93928
Abel Martin <abel.martin@gmail.com>
2011-09-18 23:31:39 -0400

    Added an example conf.js file.

---
+++ conf_example.js
@@ -0,0 +1,35 @@
+module.exports = {
+    fb: {
+        appId: 'xxx'
+      , appSecret: 'xxx'
+    }
+  , twit: {
+        consumerKey: 'xxx'
+      , consumerSecret: 'xxx'
+    }
+  , github: {
+        appId: 'xxx'
+      , appSecret: 'xxx'
+    }
+  , instagram: {
+        clientId: 'xxx'
+      , clientSecret: 'xxx'
+    }
+  , foursquare: {
+        clientId: 'xxx'
+      , clientSecret: 'xxx'
+    }
+  , gowalla: {
+        apiKey: 'xxx',
+        apiSecret: 'xxx'
+    }
+  , linkedin: {
+        apiKey: 'xxx'
+      , apiSecret: 'xxx'
+    }
+  , google: {
+        clientId: 'xxx'
+      , clientSecret: 'xxx'
+    }
+};
+
